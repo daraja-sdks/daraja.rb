cc67be5c9b89cef0dc9ee2b0cbf12416c7f74b03
Victor Ndaba <mwangindaba@gmail.com>
2022-07-20 20:12:20 +0300

    📦 feat: added routes config

--- lib/daraja/routes.rb
+++ lib/daraja/routes.rb
@@ -2,13 +2,18 @@
 
 module Daraja
   ROUTES = {
-    production: "",
-    sandbox: "",
-    oauth: "",
-    c2b_register: "",
-    c2b_simulate: "",
-    stk_push: "",
-    stk_query: "",
-    b2c: ""
+    production: "https://api.safaricom.co.ke",
+    sandbox: "https://sandbox.safaricom.co.ke",
+    oauth: "/oauth/v1/generate?grant_type=client_credentials",
+    b2c: "/mpesa/b2c/v1/paymentrequest",
+    b2b: "/mpesa/b2b/v1/paymentrequest",
+    c2bregister: "/mpesa/c2b/v1/registerurl",
+    c2bsimulate: "/mpesa/c2b/v1/simulate",
+    c2bv2simulate: "/mpesa/c2b/v2/simulate",
+    accountbalance: "/mpesa/accountbalance/v1/query",
+    transactionstatus: "/mpesa/transactionstatus/v1/query",
+    reversal: "/mpesa/reversal/v1/request",
+    stkpush: "/mpesa/stkpush/v1/processrequest",
+    stkquery: "/mpesa/stkpushquery/v1/query",
   }.freeze
 end
