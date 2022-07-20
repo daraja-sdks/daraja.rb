9d9958a4a83afd400c4703794704d0d38f525a8b
Victor Ndaba <mwangindaba@gmail.com>
2022-07-20 20:14:19 +0300

    📦 feat: started work on mpesa class functionality

--- lib/daraja/mpesa.rb
+++ lib/daraja/mpesa.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
-require_relative "./api/c2b"
+require "base64"
+require "faraday"
+require "faraday_middleware"
+require_relative "api/c2b"
+require_relative "routes"
 
 module Daraja
   class Mpesa
@@ -16,24 +20,43 @@ module Daraja
       @environment = environment
       @initiator_password = password
       @cert_path = cert_path
+      @last_token_time = 0
+      
+      base_url = @environment == "SANDBOX" ? ROUTES[:sandbox] : ROUTES[:production]
+      @connection ||= Faraday.new do |conn|
+        conn.url_prefix = base_url
+        conn.request :json
+        conn.response :json, content_type: "application/json"
+      end
+
+      yield(self) if block_given?
     end
 
-    # def connection
-    # 	base_url = @environment == "SANDBOX" ? ROUTES[:sandbox] : ROUTES[:production]
-    # 	@connection = Faraday.new do |conn|
-    # 		conn.base_url = base_url
-    # 		conn.request :json
-    # 		conn.response :json, {"Content-Type": "application/json"}
-    # 	end
-    # end
-
     def c2b
-      yield Api::CustomerToBusiness.new({ token: get_auth_token, client: "@connection" })
+      yield Api::CustomerToBusiness.new(get_builder_cfg)
     end
 
     private
-      def get_auth_token
-        "authorization_token"
+    def get_auth_token
+      if (Time.now.to_i - @last_token_time) < 3599
+        @latest_token
+      else
+        creds = Base64.urlsafe_encode64("#{@consumer_key}:#{@consumer_secret}").chomp
+        body = @connection.get(ROUTES[:oauth], {}, { Authorization: "Basic #{creds}" }).body
+        token = body["access_token"]
+  
+        @latest_token = token
+        @last_token_time = Time.now.to_i
+  
+        token
       end
+    end
+
+    def get_builder_cfg
+      {token: get_auth_token, client: @connection}
+    end
+
+    def generate_credential
+    end
   end
 end
