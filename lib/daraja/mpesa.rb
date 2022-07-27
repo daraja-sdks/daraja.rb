da67d62e189aebdff6fa311e57bc0ddddee43250
Victor Ndaba <mwangindaba@gmail.com>
2022-07-27 22:18:53 +0300

    ✨ style: formatted with rubocop

--- lib/daraja/mpesa.rb
+++ lib/daraja/mpesa.rb
@@ -21,7 +21,7 @@ module Daraja
       @initiator_password = password
       @cert_path = cert_path
       @last_token_time = 0
-      
+
       base_url = @environment == "SANDBOX" ? ROUTES[:sandbox] : ROUTES[:production]
       @connection ||= Faraday.new do |conn|
         conn.url_prefix = base_url
@@ -37,26 +37,26 @@ module Daraja
     end
 
     private
-    def get_auth_token
-      if (Time.now.to_i - @last_token_time) < 3599
-        @latest_token
-      else
-        creds = Base64.urlsafe_encode64("#{@consumer_key}:#{@consumer_secret}").chomp
-        body = @connection.get(ROUTES[:oauth], {}, { Authorization: "Basic #{creds}" }).body
-        token = body["access_token"]
-  
-        @latest_token = token
-        @last_token_time = Time.now.to_i
-  
-        token
+      def get_auth_token
+        if (Time.now.to_i - @last_token_time) < 3599
+          @latest_token
+        else
+          creds = Base64.urlsafe_encode64("#{@consumer_key}:#{@consumer_secret}").chomp
+          body = @connection.get(ROUTES[:oauth], {}, { Authorization: "Basic #{creds}" }).body
+          token = body["access_token"]
+
+          @latest_token = token
+          @last_token_time = Time.now.to_i
+
+          token
+        end
       end
-    end
 
-    def get_builder_cfg
-      {token: get_auth_token, client: @connection}
-    end
+      def get_builder_cfg
+        { token: get_auth_token, client: @connection }
+      end
 
-    def generate_credential
-    end
+      def generate_credential
+      end
   end
 end
