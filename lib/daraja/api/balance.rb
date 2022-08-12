223b76c8a837483f9034616e871b6a251614729a
Victor Ndaba <mwangindaba@gmail.com>
2022-08-12 23:09:42 +0300

    style: formatted with rubocop

--- lib/daraja/api/balance.rb
+++ lib/daraja/api/balance.rb
@@ -1,11 +1,11 @@
+# frozen_string_literal: true
+
 module Daraja
   module Api
     class AccountBalance
-
       def initialize(app)
         @app = app
       end
-
     end
   end
 end
--- lib/daraja/api/reversal.rb
+++ lib/daraja/api/reversal.rb
@@ -1,11 +1,11 @@
+# frozen_string_literal: true
+
 module Daraja
-    module Api
-        class Reversal
-
-            def initialize(app)
-              @app = app
-            end
-
-        end
+  module Api
+    class Reversal
+      def initialize(app)
+        @app = app
+      end
     end
+  end
 end
