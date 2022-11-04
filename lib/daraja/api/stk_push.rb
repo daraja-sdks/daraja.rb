672df9767cddc81078f183a831b227bf1fd35475
Victor Ndaba <mwangindaba@gmail.com>
2022-11-04 18:26:41 +0300

    feat: started work on the `stk_push` api

--- lib/daraja/api/stk_push.rb
+++ lib/daraja/api/stk_push.rb
@@ -3,7 +3,20 @@
 module Daraja
   module Api
     class STKPush
-      def initialize
+      attr_accessor :short_code, :amount, :phone_number, :callback_url, :account_ref, :lnm_passkey, :description, :transation_type
+
+      def debug_assert
+        raise "A short code has to be provided" if @short_code.nil?
+        raise "An amount has to be provided" if @amount.nil?
+        raise "A phone number must be provided" if @phone_number.nil?
+        raise "Please provide your lipaNaMpesaPasskey" if @lnm_passkey.nil?
+      end
+
+      def initialize(app)
+        @app = app
+        # Set value defaults
+        @transation_type = "CustomerPayBillOnline"
+        @amount = 1
       end
     end
   end
